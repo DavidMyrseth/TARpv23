8822ee1ff796b6cc81e9ff32d492d14c4bb8e7f4
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-09-04 11:34:21 +0300

    Update Funksioonid.cs

--- TARpv23/Funksioonid.cs
+++ TARpv23/Funksioonid.cs
@@ -1,99 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
 
-namespace TARpv23_CSharp
-{
-    internal class Funktsioonid
+namespace TARpv23_CSharp 
+    internal class Funktsioonid 
     {
-        public static void Tere(string nimi)
+        public static void Tere(string nimi) // Метод, который выводит приветствие с именем
         {
-            Console.WriteLine("Tere kallis {0}", nimi);
+            Console.WriteLine("Tere kallis {0}", nimi); // Выводит приветственное сообщение, используя имя пользователя
         }
 
-        public static int Liitmine(int arv1, int arv2)
+        public static int Liitmine(int arv1, int arv2) // Статический метод, который складывает два целых числа и возвращает результат
         {
-            return arv1 + arv2;
+            return arv1 + arv2; // Возвращает сумму двух чисел
         }
 
-        public static double Arvuta(string operatsion, int arv1, int arv2)
+        public static double Arvuta(string operatsion, int arv1, int arv2) // Статический метод для выполнения арифметических операций
         {
-            int Arve = 0;
-            if (operatsion == "+")
+            double Arve = 0; // Инициализация переменной для хранения результата операции
+            if (operatsion == "+") // Проверка, является ли операция сложением
             {
-                Arve = arv1 + arv2;
+                Arve = arv1 + arv2; // Выполняется сложение
             }
-            else if (operatsion == "-")
+            else if (operatsion == "-") // Проверка, является ли операция вычитанием
             {
-                Arve = arv1 - arv2;
+                Arve = arv1 - arv2; // Выполняется вычитание
             }
-            else if (operatsion == "/")
+            else if (operatsion == "/") // Проверка, является ли операция делением
             {
-                Arve = arv1 / arv2;
+                Arve = (double)arv1 / arv2; // Выполняется деление, приведение к типу double для правильного деления
             }
-            else if (operatsion == "*")
+            else if (operatsion == "*") // Проверка, является ли операция умножением
             {
-                Arve = arv1 * arv2;
+                Arve = arv1 * arv2; // Выполняется умножение
             }
-            return Arve;
+            return Arve; // Возвращает результат операции
         }
-        //Päeva number-> päeva nimetus
-        public static string Paevad(int nr)
+
+        // Метод для получения названия дня недели по его номеру
+        public static string Paevad(int nr) 
         {
-            string paev;
-            switch (nr)
+            string paev; // Переменная для хранения названия дня
+            switch (nr) // Выбор дня недели в зависимости от номера
             {
-                case 1: paev = "Esmaspäev"; break;
-                case 2: paev = "Teisipäev"; break;
-                case 3: paev = "Kolmapäev"; break;
-                case 4: paev = "Neljapäev"; break;
-                case 5: paev = "Reede"; break;
-                case 6: paev = "Laulapäev"; break;
-                case 7: paev = "Pühapäev"; break;
-                default:
-                    paev = "Ei saa määrata";
+                case 1: paev = "Esmaspäev"; break; // Если 1, то день - понедельник
+                case 2: paev = "Teisipäev"; break; // Если 2, то день - вторник
+                case 3: paev = "Kolmapäev"; break; // Если 3, то день - среда
+                case 4: paev = "Neljapäev"; break; // Если 4, то день - четверг
+                case 5: paev = "Reede"; break; // Если 5, то день - пятница
+                case 6: paev = "Laupäev"; break; // Если 6, то день - суббота (исправлено на правильное название)
+                case 7: paev = "Pühapäev"; break; // Если 7, то день - воскресенье
+                default: // Если номер не соответствует ни одному из дней
+                    paev = "Ei saa määrata"; // Сообщение об ошибке
                     break;
             }
-            return paev;
-
-
+            return paev; // Возвращает название дня недели
         }
-        public static string Pikkuse_analuus(double pikkus)
+
+        // Метод для анализа роста человека
+        public static string Pikkuse_analuus(double pikkus) 
         {
-            string analuus;
-            if (pikkus < 1.6)
+            string analuus; // Переменная для хранения результата анализа
+            if (pikkus < 1.6) // Если рост меньше 1.6 метра
             {
-                analuus = "Lühike kasvu inimene";
+                analuus = "Lühike kasvu inimene"; // Возвращаем результат, что человек невысокого роста
             }
-            else if (pikkus < 1.8)
+            else if (pikkus < 1.8) // Если рост меньше 1.8 метра, но больше или равно 1.6 метра
             {
-                analuus = "Keskmine kasvu inimene";
+                analuus = "Keskmine kasvu inimene"; // Возвращаем результат, что человек среднего роста
             }
-            else
+            else // Если рост 1.8 метра или больше
             {
-                analuus = "Pikka kasvu inimene";
+                analuus = "Pikka kasvu inimene"; // Возвращаем результат, что человек высокого роста
             }
-            return analuus;
+            return analuus; // Возвращаем результат анализа
         }
-        public static int[] Arvude_massive(int N, int M)
+
+        // Метод для создания массива чисел от N до M
+        public static int[] Arvude_massive(int N, int M) 
         {
-            int arv;
-            int[] arvud = new int[M - N];
-            for (int i = 0; i < arvud.Length; i++)
+            int[] arvud = new int[M - N + 1]; // Создаем массив размером от M до N (исправлено на корректный размер массива)
+            for (int i = 0; i < arvud.Length; i++) // Цикл по всему массиву
             {
-                Console.WriteLine("{0} nimi :", i + 1);
-                arv = N;
-                arvud[i] = arv;
-                N++;
+                arvud[i] = N; // Заполняем массив значениями, начиная с N
+                N++; // Увеличиваем значение N на 1
             }
-            return arvud;
-
-
-
-
+            return arvud; // Возвращаем массив чисел
         }
     }
 }
